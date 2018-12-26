6f7fd5d7cb17f832f03c52818ffe4fabeceb996e
edwardyi <yi.wei.jie@hotmail.com>
2018-12-26 16:44:46 +0800

    add ChatScreen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
 import UsernameForm from './components/UsernameForm';
+import ChatScreen from './components/ChatScreen';
 
 class App extends Component {
+	constructor(){
+		super();
+		this.onUserSubmitted = this.onUserSubmitted.bind(this);
+		this.state = {
+			whereAreYou: 'usernameform'
+		};
+	}
+	onUserSubmitted(username){
+		// curl -H "Content-type: application/json" -X POST -d '{"username":"newuser"}' http://localhost:3001/users
+		fetch('http://localhost:3001/users', {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: JSON.stringify({username})
+		}).then(()=> { 
+			this.setState({
+				whereAreYou : 'chatSCreen'
+			})
+			console.log(`${username}成功進入聊天室`)
+		})
+		.catch(err => { console.log('POST請求失敗')})
+	}
 	render() {
-		return (
-			<UsernameForm onSubmit={username=>alert(username)} />
-		);
+		if (this.state.whereAreYou === 'usernameform') {
+			return (<UsernameForm onSubmit={this.onUserSubmitted} />);
+		} else {
+			return (<ChatScreen />)
+		}
 	}
 }
 
---
+++ src/components/ChatScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class ChatScreen extends React.Component{
+  constructor(){
+    super();
+  }
+  render(){
+    return(
+      'Chat Begin'
+    )
+  }
+}
+
+export default ChatScreen;
